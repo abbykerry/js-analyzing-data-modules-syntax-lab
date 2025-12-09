85c8247e657af6945e1590f4ac2e391c5853fec8
Abigael Ongwae <abbykerry17@gmail.com>
2025-12-09 15:48:34 +0300

    modified indeex.js

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
+require('datejs');
+function combineUsers(...args){
+  const combineObject = {
+    users : []
+  }
 
+for(const eacharr of args){
+  combineObject.users.push(...eacharr);
+}
+combineObject.merge_date = Date.today().toString("M/d/yyyy");
+  return combineObject;
+}
 
 
 module.exports = {
